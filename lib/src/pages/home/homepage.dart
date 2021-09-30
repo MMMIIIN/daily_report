a731c486428e555d8bebbcde9b0175b4f55f12b3
MMMIIIN <wbsldj7645@naver.com>
2021-09-30 21:39:52 +0900

    홈 화면 dialog 추가

--- lib/src/pages/home/homepage.dart
+++ lib/src/pages/home/homepage.dart
@@ -1,15 +1,21 @@
 import 'package:daily_report/color.dart';
+import 'package:daily_report/icons.dart';
 import 'package:daily_report/src/data/todo/todo_controller.dart';
+import 'package:daily_report/src/pages/chart/controller/chart_controller.dart';
+import 'package:daily_report/src/pages/list/add_todo.dart';
 import 'package:daily_report/src/pages/settings/controller/settings_controller.dart';
+import 'package:daily_report/src/service/firestore_service.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:time_range_picker/time_range_picker.dart';
 
 final TodoController _todoController = Get.put(TodoController());
 final SettingsController _settingsController = Get.put(SettingsController());
+final ChartController _chartController = Get.put(ChartController());
 
 class HomePage extends StatefulWidget {
   @override
@@ -26,6 +32,11 @@ class _HomePageState extends State<HomePage> {
     if (!isSameDay(_selectedDay, selectedDay)) {
       setState(() {
         _selectedDay = selectedDay;
+        _todoController.initHome(_selectedDay);
+        _todoController.selectDateTime(_selectedDay);
+      });
+    } else {
+      setState(() {
         _todoController.initHome(_selectedDay);
         _todoController.selectDateTime(_selectedDay);
       });
@@ -184,7 +195,9 @@ class _HomePageState extends State<HomePage> {
                   sections: List<PieChartSectionData>.generate(
                       _todoController.currentIndexList.length, (index) {
                     final isTouched = index == touchedIndex;
-                    final radius = isTouched ? context.mediaQuery.size.width * 0.17 : context.mediaQuery.size.width * 0.14;
+                    final radius = isTouched
+                        ? context.mediaQuery.size.width * 0.17
+                        : context.mediaQuery.size.width * 0.14;
                     return PieChartSectionData(
                       title:
                           '${_todoController.currentUidList.value.todoList[index].percent.toStringAsFixed(0)}%',
@@ -211,62 +224,93 @@ class _HomePageState extends State<HomePage> {
         builder: (_) => _todoController.currentIndexList.isNotEmpty
             ? ListView.builder(
                 itemCount: _todoController.currentIndexList.length,
-                itemBuilder: (context, index) => Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 17,
-                          height: 17,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: colorList[_todoController.currentUidList
-                                  .value.todoList[index].colorIndex]),
+                itemBuilder: (context, index) => InkWell(
+                  borderRadius: BorderRadius.circular(10),
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (_) => todoDialog(
+                        _todoController
+                            .currentUidList.value.todoList[index].colorIndex,
+                        _todoController
+                            .currentUidList.value.todoList[index].title,
+                        _todoController
+                            .currentUidList.value.todoList[index].ymd,
+                        _todoController
+                            .currentUidList.value.todoList[index].memoText,
+                        TimeRange(
+                          startTime: TimeOfDay(
+                              hour: _todoController.currentUidList.value
+                                  .todoList[index].startHour,
+                              minute: _todoController.currentUidList.value
+                                  .todoList[index].startMinute),
+                          endTime: TimeOfDay(
+                              hour: _todoController
+                                  .currentUidList.value.todoList[index].endHour,
+                              minute: _todoController.currentUidList.value
+                                  .todoList[index].endMinute),
                         ),
-                        SizedBox(width: 10),
-                        Container(
-                          width: context.mediaQuery.size.width * 0.8,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                width: context.mediaQuery.size.width * 0.3,
-                                child: Text(
-                                  _todoController.currentUidList.value
-                                      .todoList[index].title,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              Container(
-                                alignment: Alignment.center,
-                                width: context.mediaQuery.size.width * 0.3,
-                                child: Text(
-                                  _todoController.currentUidList.value
-                                      .todoList[index].memoText,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                              Container(
-                                alignment: Alignment.topRight,
-                                width: context.mediaQuery.size.width * 0.2,
-                                child: isPercentOrHour
-                                    ? Text(
-                                        ' ${_todoController.currentUidList.value.todoList[index].hourMinute}')
-                                    : Text(
-                                        ' ${_todoController.currentUidList.value.todoList[index].percent.toStringAsFixed(0)} %',
-                                        style: TextStyle(fontSize: 16),
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                              )
-                            ],
+                        _todoController.currentUidList.value.todoList[index].uid
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 17,
+                            height: 17,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: colorList[_todoController.currentUidList
+                                    .value.todoList[index].colorIndex]),
                           ),
-                        ),
-                      ],
+                          SizedBox(width: 10),
+                          Container(
+                            width: context.mediaQuery.size.width * 0.8,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  width: context.mediaQuery.size.width * 0.3,
+                                  child: Text(
+                                    _todoController.currentUidList.value
+                                        .todoList[index].title,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  width: context.mediaQuery.size.width * 0.3,
+                                  child: Text(
+                                    _todoController.currentUidList.value
+                                        .todoList[index].memoText,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.topRight,
+                                  width: context.mediaQuery.size.width * 0.2,
+                                  child: isPercentOrHour
+                                      ? Text(
+                                          ' ${_todoController.currentUidList.value.todoList[index].hourMinute}')
+                                      : Text(
+                                          ' ${_todoController.currentUidList.value.todoList[index].percent.toStringAsFixed(0)} %',
+                                          style: TextStyle(fontSize: 16),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -274,5 +318,194 @@ class _HomePageState extends State<HomePage> {
             : Container(),
       ),
     );
+  }
+
+  Widget todoDialog(int colorIndex, String title, DateTime dateTime,
+      String memoText, TimeRange timeRange, String todoUid) {
+    return AlertDialog(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: colorList[colorIndex]),
+              ),
+              SizedBox(width: 10),
+              Container(
+                  width: context.mediaQuery.size.width * 0.4,
+                  child: Text(
+                    title,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+            ],
+          ),
+          IconButton(icon: Icon(IconsDB.trash),
+              splashRadius: 20,
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('삭제 하시겠습니까?'),
+                      actions: [
+                        InkWell(
+                          splashColor:
+                          context.theme.primaryColor.withOpacity(0.4),
+                          highlightColor:
+                          context.theme.primaryColor.withOpacity(0.2),
+                          onTap: () {
+                            Get.back();
+                          },
+                          child: Container(
+                            width: context.mediaQuery.size.width * 0.17,
+                            height: context.mediaQuery.size.height * 0.043,
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: context.theme.primaryColor)),
+                            child: Center(
+                              child: Text(
+                                '취소',
+                                style: TextStyle(
+                                    color: context.theme.primaryColor),
+                              ),
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () async {
+                            _todoController.todoDelete(todoUid);
+                            await todoFirebaseDelete(todoUid).then((value) {
+                              _chartController.makeRangeDate();
+                              _todoController.setCurrentIndex(
+                                  _todoController.currentDateTime.value);
+                            });
+                            Get.back();
+                            Get.back();
+                            setState(() {});
+                            await Get.showSnackbar(
+                              GetBar(
+                                duration: Duration(seconds: 2),
+                                title: 'SUCCESS',
+                                message: '성공적으로 삭제되었습니다.',
+                                backgroundColor: successColor,
+                              ),
+                            );
+                          },
+                          child: Container(
+                            width: context.mediaQuery.size.width * 0.17,
+                            height: context.mediaQuery.size.height * 0.043,
+                            decoration: BoxDecoration(
+                                color: context.theme.primaryColor),
+                            child: Center(
+                              child: Text(
+                                '삭제',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              })
+        ],
+      ),
+      content: Container(
+        height: memoText.isEmpty
+            ? context.mediaQuery.size.height * 0.12
+            : context.mediaQuery.size.height * 0.15,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  '${dateTime.year}.'
+                  '${dateTime.month}.'
+                  '${dateTime.day} '
+                  '${_todoController.getOfDay(dateTime.weekday)}',
+                  style: TextStyle(fontSize: 30),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            memoText.isEmpty
+                ? SizedBox()
+                : Text(
+                    memoText,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 25),
+                  ),
+            SizedBox(height: 10),
+            Text(
+              '${timeRange.startTime.hour} : '
+              '${timeRange.startTime.minute} - '
+              ' ${timeRange.endTime.hour} : '
+              '${timeRange.endTime.minute}',
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ),
+      ),
+      actions: [
+        InkWell(
+          splashColor: context.theme.primaryColor.withOpacity(0.4),
+          highlightColor: context.theme.primaryColor.withOpacity(0.2),
+          onTap: () {
+            Get.back();
+          },
+          child: Container(
+            width: context.mediaQuery.size.width * 0.17,
+            height: context.mediaQuery.size.height * 0.043,
+            decoration: BoxDecoration(
+                border: Border.all(color: context.theme.primaryColor)),
+            child: Center(
+              child: Text(
+                '취소',
+                style: TextStyle(color: context.theme.primaryColor),
+              ),
+            ),
+          ),
+        ),
+        InkWell(
+          onTap: () {
+            _todoController.currentDateTime(dateTime);
+            _todoController.titleTextController.value.text = title;
+            _todoController.titleText(title);
+            _todoController.memoController.value.text = memoText;
+            _todoController.memoText(memoText);
+            _todoController.setTime(timeRange);
+            _todoController.selectColorIndex(colorIndex);
+            _todoController.isEditMode(true);
+            _todoController.editTodoUid(todoUid);
+            Get.to(() => AddTodo());
+          },
+          child: Container(
+            width: context.mediaQuery.size.width * 0.17,
+            height: context.mediaQuery.size.height * 0.043,
+            color: context.theme.primaryColor,
+            child: Center(
+              child: Text(
+                '수정',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
   }
 }
