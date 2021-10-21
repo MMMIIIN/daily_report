fa1ffac1c67d6b5ed1256af2e183cbfe0fb9f0b6
MMMIIIN <wbsldj7645@naver.com>
2021-10-21 23:22:38 +0900

    selectTime(2)

--- lib/src/pages/add/select_time.dart
+++ lib/src/pages/add/select_time.dart
@@ -1,10 +1,13 @@
 import 'package:daily_report/src/data/todo/todo_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
 import 'package:get/get.dart';
 import 'package:time_range_picker/time_range_picker.dart';
 
 final TodoController _todoController = Get.put(TodoController());
+final FocusNode _startHourFocus = FocusNode();
+final FocusNode _startMinuteFocus = FocusNode();
+final FocusNode _endHourFocus = FocusNode();
+final FocusNode _endMinuteFocus = FocusNode();
 
 class SelectTimePage extends StatefulWidget {
   @override
@@ -18,123 +21,7 @@ class _SelectTimePageState extends State<SelectTimePage> {
       child: SafeArea(
         child: Column(
           children: [
-            Obx(
-              () => Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  GestureDetector(
-                    onTap: () async {
-                      _todoController.startTime(
-                        await showTimePicker(
-                          context: context,
-                          initialTime: _todoController.startTime.value,
-                          initialEntryMode: TimePickerEntryMode.input,
-                        ),
-                      );
-                    },
-                    child: Container(
-                      width: context.mediaQuery.size.width * 0.35,
-                      height: context.mediaQuery.size.height * 0.07,
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                          child:
-                              Text('${_todoController.startTime.value.hour} : '
-                                  '${_todoController.startTime.value.minute}')),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () async {
-                      _todoController.endTime(
-                        await showTimePicker(
-                          context: context,
-                          initialTime: _todoController.endTime.value,
-                          initialEntryMode: TimePickerEntryMode.input,
-                        ),
-                      );
-                    },
-                    child: Container(
-                      width: context.mediaQuery.size.width * 0.35,
-                      height: context.mediaQuery.size.height * 0.07,
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text('${_todoController.endTime.value.hour} : '
-                            '${_todoController.endTime.value.minute}'),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  decoration: BoxDecoration(border: Border.all()),
-                  child: TimePickerSpinner(
-                    time: DateTime(
-                        2021,
-                        1,
-                        1,
-                        _todoController.startTime.value.hour,
-                        _todoController.startTime.value.minute),
-                    itemWidth: 40,
-                    spacing: 10,
-                    itemHeight: 20,
-                    normalTextStyle: TextStyle(fontSize: 20),
-                    highlightedTextStyle: TextStyle(
-                        fontSize: 20,
-                        color: context.theme.primaryColor,
-                        fontWeight: FontWeight.bold),
-                    alignment: Alignment.center,
-                    isForce2Digits: true,
-                    // is24HourMode: false,
-                    minutesInterval: 10,
-                    onTimeChange: (time) {
-                      setState(() {
-                        _todoController.startTime(
-                            TimeOfDay(hour: time.hour, minute: time.minute));
-                      });
-                    },
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(border: Border.all()),
-                  child: TimePickerSpinner(
-                    time: DateTime(
-                        2021,
-                        1,
-                        1,
-                        _todoController.endTime.value.hour,
-                        _todoController.endTime.value.minute),
-                    spacing: 10,
-                    itemHeight: 20,
-                    normalTextStyle: TextStyle(fontSize: 20),
-                    highlightedTextStyle:
-                        TextStyle(fontSize: 20, color: Colors.red),
-                    alignment: Alignment.center,
-                    isForce2Digits: true,
-                    // is24HourMode: false,
-                    minutesInterval: 10,
-                    onTimeChange: (time) {
-                      setState(
-                        () {
-                          _todoController.endTime(
-                            TimeOfDay(hour: time.hour, minute: time.minute),
-                          );
-                        },
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-            inputTimeField(),
+            selectTime(),
             MaterialButton(
               onPressed: () {
                 var result = _todoController.getValue(
@@ -152,12 +39,85 @@ class _SelectTimePageState extends State<SelectTimePage> {
     );
   }
 
+  Widget selectTime() {
+    return Row(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            width: context.mediaQuery.size.width * 0.6,
+            height: context.mediaQuery.size.height * 0.1,
+            decoration: BoxDecoration(
+                border: Border.all(), borderRadius: BorderRadius.circular(10)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _todoController.selectTime.value == 0
+                    ? Container(
+                        width: context.mediaQuery.size.width * 0.5,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('${_todoController.startTime.value.hour} : '
+                                '${_todoController.startTime.value.minute}'),
+                            Text('${_todoController.endTime.value.hour} : '
+                                '${_todoController.endTime.value.minute}'),
+                          ],
+                        ),
+                      )
+                    : inputTimeField(),
+              ],
+            ),
+          ),
+          Container(
+            width: context.mediaQuery.size.width * 0.2,
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    _todoController.selectTime(0);
+                  },
+                  child: Container(
+                    child: Center(child: Text('시계')),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    _todoController.selectTime(1);
+                  },
+                  child: Container(
+                    child: Center(child: Text('직접 입력')),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: context.mediaQuery.size.width * 0.05,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.check,
+                    color: _todoController.selectTime.value == 0
+                        ? context.theme.primaryColor
+                        : Colors.transparent),
+                Icon(Icons.check,
+                    color: _todoController.selectTime.value == 1
+                        ? context.theme.primaryColor
+                        : Colors.transparent),
+              ],
+            ),
+          ),
+        ],
+    );
+  }
+
   Widget inputTimeField() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         Container(
-          width: context.width * 0.3,
+          width: context.mediaQuery.size.width * 0.3,
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.end,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -165,36 +125,53 @@ class _SelectTimePageState extends State<SelectTimePage> {
               Container(
                 width: context.mediaQuery.size.width * 0.1,
                 child: TextField(
+                  focusNode: _startHourFocus,
                   textAlign: TextAlign.center,
                   onChanged: (number) {
-                    _todoController.startHour(int.parse(number));
+                    if (number.isNotEmpty) {
+                      _todoController.startHour(int.parse(number));
+                    }
+                    if (number.length == 2) {
+                      _fieldFocusChange(
+                          context, _startHourFocus, _startMinuteFocus);
+                    }
                   },
                   maxLength: 2,
-                  decoration: InputDecoration(counterText: ''),
+                  decoration: InputDecoration(
+                      counterText: '',
+                      contentPadding: EdgeInsets.only(top: 20)),
                   keyboardType: TextInputType.number,
                   textInputAction: TextInputAction.next,
                 ),
               ),
               Text(
                 ':',
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
               ),
               Container(
                 width: context.mediaQuery.size.width * 0.1,
                 child: TextField(
+                  focusNode: _startMinuteFocus,
                   textAlign: TextAlign.center,
                   onChanged: (number) {
-                    _todoController.startMinute(int.parse(number));
+                    if (number.isNotEmpty) {
+                      _todoController.startMinute(int.parse(number));
+                    }
+                    if (number.length == 2) {
+                      _fieldFocusChange(
+                          context, _startMinuteFocus, _endHourFocus);
+                      _todoController.startTime(
+                        TimeOfDay(
+                            hour: _todoController.startHour.value,
+                            minute: _todoController.startMinute.value),
+                      );
+                    }
                   },
-                  onSubmitted: (text) {
-                    _todoController.startTime(
-                      TimeOfDay(
-                          hour: _todoController.startHour.value,
-                          minute: _todoController.startMinute.value),
-                    );
-                  },
+                  onSubmitted: (text) {},
                   maxLength: 2,
-                  decoration: InputDecoration(counterText: ''),
+                  decoration: InputDecoration(
+                      counterText: '',
+                      contentPadding: EdgeInsets.only(top: 20)),
                   keyboardType: TextInputType.number,
                   textInputAction: TextInputAction.next,
                 ),
@@ -203,7 +180,7 @@ class _SelectTimePageState extends State<SelectTimePage> {
           ),
         ),
         Container(
-          width: context.width * 0.3,
+          width: context.mediaQuery.size.width * 0.3,
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.end,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -211,29 +188,61 @@ class _SelectTimePageState extends State<SelectTimePage> {
               Container(
                 width: context.mediaQuery.size.width * 0.1,
                 child: TextField(
+                  focusNode: _endHourFocus,
                   textAlign: TextAlign.center,
                   onChanged: (number) {
-                    _todoController.endHour(int.parse(number));
+                    if (number.isNotEmpty) {
+                      _todoController.endHour(int.parse(number));
+                    }
+                    if (number.length == 2) {
+                      _fieldFocusChange(
+                          context, _endHourFocus, _endMinuteFocus);
+                    }
                   },
                   maxLength: 2,
-                  decoration: InputDecoration(counterText: ''),
+                  decoration: InputDecoration(
+                      counterText: '',
+                      contentPadding: EdgeInsets.only(top: 20)),
                   keyboardType: TextInputType.number,
                   textInputAction: TextInputAction.next,
                 ),
               ),
               Text(
                 ':',
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
               ),
               Container(
                 width: context.mediaQuery.size.width * 0.1,
                 child: TextField(
+                  focusNode: _endMinuteFocus,
                   textAlign: TextAlign.center,
                   onChanged: (number) {
-                    _todoController.endMinute(int.parse(number));
+                    if (number.isNotEmpty) {
+                      _todoController.endMinute(int.parse(number));
+                    }
+                    if (number.length == 2) {
+                      _endMinuteFocus.unfocus();
+                      _todoController.endTime(TimeOfDay(
+                          hour: _todoController.endHour.value,
+                          minute: _todoController.endMinute.value));
+                      var result = _todoController.getValue(
+                        DateTime.now(),
+                        TimeRange(
+                          startTime: TimeOfDay(
+                              hour: _todoController.startHour.value,
+                              minute: _todoController.startMinute.value),
+                          endTime: TimeOfDay(
+                              hour: _todoController.endHour.value,
+                              minute: _todoController.endMinute.value),
+                        ),
+                      );
+                      print(result);
+                    }
                   },
                   maxLength: 2,
-                  decoration: InputDecoration(counterText: ''),
+                  decoration: InputDecoration(
+                      counterText: '',
+                      contentPadding: EdgeInsets.only(top: 20)),
                   keyboardType: TextInputType.number,
                   textInputAction: TextInputAction.next,
                   onSubmitted: (number) {
@@ -260,5 +269,11 @@ class _SelectTimePageState extends State<SelectTimePage> {
         ),
       ],
     );
+  }
+
+  void _fieldFocusChange(
+      BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
   }
 }
